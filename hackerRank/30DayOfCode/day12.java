10f8f140970ca04e7c4eb5069595204c689a2f06
Jinhuyk Park <pjh08190819@naver.com>
2018-02-11 23:56:22 +0900

    test

--- hackerRank/30DayOfCode/day12.java
+++ hackerRank/30DayOfCode/day12.java
@@ -18,24 +18,6 @@ class Person {
                 "Name: " + lastName + ", " + firstName
                         + 	"\nID: " + idNumber);
     }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String firstName = scan.next();
-        String lastName = scan.next();
-        int id = scan.nextInt();
-        int numScores = scan.nextInt();
-        int[] testScores = new int[numScores];
-        for(int i = 0; i < numScores; i++){
-            testScores[i] = scan.nextInt();
-        }
-        scan.close();
-
-        Student s = new Student(firstName, lastName, id, testScores);
-        s.printPerson();
-        System.out.println("Grade: " + s.calculate() );
-    }
-
 }
 class Student extends Person{
     private int[] testScores;
@@ -76,4 +58,22 @@ class Student extends Person{
         }
     }
 
+}
+public day12{
+  public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      String firstName = scan.next();
+      String lastName = scan.next();
+      int id = scan.nextInt();
+      int numScores = scan.nextInt();
+      int[] testScores = new int[numScores];
+      for(int i = 0; i < numScores; i++){
+          testScores[i] = scan.nextInt();
+      }
+      scan.close();
+
+      Student s = new Student(firstName, lastName, id, testScores);
+      s.printPerson();
+      System.out.println("Grade: " + s.calculate() );
+  }
 }
