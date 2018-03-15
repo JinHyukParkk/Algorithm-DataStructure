e5f1a28a1f3dcb6aff8e0630779fbd003bba9664
Jinhuyk Park <pjh08190819@naver.com>
2018-03-15 23:17:04 +0900

    11727 add

---
+++ boj/p11727.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class p11727 {
+    Myscanner sc = new Myscanner();
+    PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    StringBuilder sb = new StringBuilder("");
+
+    // using value
+    int n;
+    int dp[];
+
+    void start(){
+        try{
+            input();
+            sovle();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    void input() throws IOException {
+        n = sc.nextInt();
+        dp = new int[n+1];
+    }
+    void sovle() {
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 5;
+
+        for (int i = 4; i <=n; i++) {
+            dp[i] = 2*dp[i-2]+dp[i-1];
+            dp[i] %= 10007;
+        }
+        System.out.println(dp[n]);
+    }
+    class Myscanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        Myscanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+    public static void main(String[] args) {
+        new p11727().start();
+    }
+}
