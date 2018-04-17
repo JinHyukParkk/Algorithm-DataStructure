a5ea997b56c21298397bb17f0e35affdcc51cf8c
Jinhuyk Park <pjh08190819@naver.com>
2018-04-17 15:41:43 +0900

    "04.17 Tuse"

--- boj/p2805.java
+++ boj/p2805.java
@@ -11,6 +11,9 @@ public class p2805 {
 
 
         int arr[];
+        int right = 0;
+        int left = 0;
+
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         arr = new int[n];
@@ -19,28 +22,23 @@ public class p2805 {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            right = right<arr[i]?arr[i]:right;
         }
-
-        Arrays.sort(arr);
-        int right = arr[n-1];
-        int left = 0;
+        int ans = 0;
         int mid = 0;
-        while(left<=right){
+        while(right-left>1){
+            long sum = 0L;
             mid = (left+right)/2;
-
-            int cut = 0;
-            long sum = 0;
             for (int i = 0; i < arr.length; i++) {
-                cut = arr[i]- mid;
-                cut = cut<0?0:cut;
-                sum+=cut;
+                sum += arr[i]-mid<0?0:arr[i]-mid;
             }
             if(sum >= m){
-                left = mid+1;
-            }else if(sum < m){
-                right = mid-1;
+                ans = ans<mid?mid:ans;
+                left = mid;
+            }else {
+                right = mid;
             }
         }
-        System.out.println(right);
+        System.out.println(ans);
     }
 }
