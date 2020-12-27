1e4c553257c320f41a3c698f00169332c0fadf59
hyuk <pjh08190819@naver.com>
2020-12-27 20:52:07 +0900

    DrawCrain algo2

--- programmers/DrawCrain.java
+++ programmers/DrawCrain.java
@@ -56,4 +56,27 @@ public class DrawCrain {
 
         return ans*2;
     }
+
+    public int solution1(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int m : moves) {
+            for(int i=0; i<board.length; ++i) {
+                if(board[i][m-1] == 0) continue;
+
+                if(!stack.isEmpty() && stack.peek() == board[i][m-1]) {
+                    stack.pop();
+                    answer += 2;
+                } else {
+                    stack.push(board[i][m-1]);
+                }
+
+                board[i][m-1] = 0;
+                break;
+            }
+        }
+
+        return answer;
+    }
 }
