0520cd83377e0ae0129c066d7eab44422190dbb4
hyuk <pjh08190819@naver.com>
2020-12-27 20:49:25 +0900

    DrawCrain algo

---
+++ programmers/DrawCrain.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+
+public class DrawCrain {
+    public static void main(String args[]) {
+        DrawCrain dc = new DrawCrain();
+
+        int board[][] = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int moves[] = {1,5,3,5,1,2,1,4};
+
+        System.out.println(dc.solution(board, moves));
+    }
+
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> s = new Stack<>();
+        int size = board.length;
+        int pivot[] = new int[size];
+        
+        int returnValue, addValue = 0;
+        int ans = 0;
+        
+        // 높이 설정 
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[j][i] != 0) {
+                    pivot[i] = j;
+                    break;
+                }
+            }
+        }
+
+        for (int i : moves) {
+            int j = i - 1;
+            // 열은 비워 있음
+            if (pivot[j] == size) {
+                continue;
+            }
+
+            // 추가 될 값
+            addValue = board[pivot[j]][j];
+            pivot[j]++;
+
+            // 아무 것도 없을 때 
+            if (s.isEmpty()) {
+                s.push(addValue);
+                continue;
+            }
+            
+            returnValue = s.peek();
+            if (addValue == returnValue) {
+                s.pop();
+                ans++;
+            } else {
+                s.push(addValue);
+            }
+        }
+
+        return ans*2;
+    }
+}
