ba44b7f7e857ca310de358e22846bd64f75f6175
hyuk <pjh08190819@naver.com>
2021-02-09 00:15:11 +0900

    캡슐화 테스트

--- etc/main.go
+++ etc/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"Algorithm_DataStructure/etc/algo"
+	// "Algorithm_DataStructure/etc/algo"
 	"Algorithm_DataStructure/etc/oop"
+	"fmt"
 )
+
 func main() {
-	algo.Multiplication()
+	// algo.Multiplication()
+
+	person := oop.NewPerson("hyuk")
+	fmt.Println(person.GetName())
+	person.SetName("hyuk2")
+	fmt.Println(person.GetName())
+
 }
--- etc/oop/Encapsulation.go
+++ etc/oop/Encapsulation.go
@@ -4,17 +4,17 @@ type Person struct {
 	name string
 }
 
-// 생성자 
+// 생성자
 func NewPerson(name string) *Person {
-	return &Person{ name: name}
+	return &Person{name: name}
 }
 
 // 캡슐화
-func (person Person) Getname() string {
+func (person Person) GetName() string {
 	return person.name
 }
 
 // set의 경우 포인터 필요
-func (person *Person) Setname(name string) {
+func (person *Person) SetName(name string) {
 	person.name = name
 }
