675b7c3fc31616256833e186c473cb83fa10afde
hyuk <pjh08190819@naver.com>
2021-03-01 23:46:23 +0900

    newid problem

---
+++ programmers/NewId.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jinhyuk on 2021. 03. 01..
+ */
+import java.util.Arrays;
+
+public class NewId {
+
+    public String solution(String new_id){
+        String answer = "";
+        return answer;
+    }
+    public static void main(String[] args) {
+        NewId ni = new NewId();
+        //아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println((ni.solution("=.=")));
+    }
+
+}
