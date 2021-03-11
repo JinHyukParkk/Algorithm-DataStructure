94fe73ffdf04a245d5cfbbae51f9f830e35a6c99
hyuk <pjh08190819@naver.com>
2021-03-12 00:22:17 +0900

    3집법 뒤집기

--- programmers/FilpTernary.java
+++ programmers/FilpTernary.java
@@ -1,13 +1,19 @@
 public class FilpTernary {
     public int solution(int n) {
+        // 뒤집어도 이 계산법이 됨.
         int threeNum = 0;
-        while()
-        return answer;
+
+        while(n > 2) {
+            threeNum = (threeNum + (n % 3)) * 3;
+            n /= 3;
+        }
+
+        return threeNum + n;
     }
 
     public static void main(String[] args) {
         FilpTernary ft = new FilpTernary();
         //아래는 테스트로 출력해 보기 위한 코드입니다.
-        System.out.println(ft.solution(45));
+        System.out.println(ft.solution(125));
     }
 }
