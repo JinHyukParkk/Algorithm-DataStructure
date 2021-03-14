670bd8cb89ccdd384bb77a6e4f311cc61d3fb4f2
hyuk <pjh08190819@naver.com>
2021-03-14 22:10:06 +0900

    폰켓몬

---
+++ programmers/PhoneCatMon.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class PhoneCatMon {
+    public int solution(int[] nums) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        al.add(nums[0]);
+        for(int i = 1; i < nums.length; i++) {
+            if(!al.contains(nums[i])) {
+                al.add(nums[i]);
+            }
+        }
+
+        int answer = Math.min(nums.length/2, al.size());
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PhoneCatMon pcm = new PhoneCatMon();
+        //아래는 테스트로 출력해 보기 위한 코드입니다.
+        int test[] = {3,3,3,2,2,4};
+        int ans = pcm.solution(test);
+        System.out.println(ans);
+       
+    }
+}
