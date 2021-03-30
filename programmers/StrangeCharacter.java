7f69103f55434ca939bf436430a675daaef38dec
hyuk <pjh08190819@naver.com>
2021-03-30 23:40:27 +0900

    이상한 문자열 만들기

---
+++ programmers/StrangeCharacter.java
@@ -0,0 +1,39 @@
+public class StrangeCharacter {
+    public String solution(String s) {
+        String answer = "";
+        
+        String str[] = s.split("");
+        String space = " ";
+        int cnt = 0;
+        
+        for(int i = 0; i < str.length; i++){
+            if(str[i].equals(space)){
+               cnt = 0;
+            }else{
+               if(cnt % 2 == 0){
+                  cnt++;
+                  str[i] = str[i].toUpperCase();
+               }else{
+                   cnt++;
+                   str[i] = str[i].toLowerCase();
+               }
+            }
+            answer += str[i];
+        }
+        
+        System.out.println(answer);
+        
+        return answer;
+  
+  
+        public static void main(String[] args) {
+            StrangeCharacter ss = new StrangeCharacter();
+            //아래는 테스트로 출력해 보기 위한 코드입니다.
+            String test[] = {"sun", "bed", "car"};
+            String[] ans = ss.solution(test, 1);
+            for (int i = 0; i < ans.length; i++) {
+                System.out.println(ans[i]);
+            }
+            
+        }
+}
