12e98797f65c31b3cd7f9afd74b0b754da24664c
hyuk <pjh08190819@naver.com>
2022-03-15 00:36:21 +0900

    feat : AbcProblem 문제 풀기풀기

--- codility/AbcProblem.java
+++ codility/AbcProblem.java
@@ -1,19 +1,42 @@
+import java.io.CharArrayReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class AbcProblem {
     public static final int MAX_VALUE = 100001;
 
     public int solution(String[] colors) {
 
-        String[] givenColors = {"aab", "cab", "baa", "baa"};
+        HashMap<Character, Integer> count = new HashMap<>();
+        int answer = 0;
 
-        for(int i = 0; i < colors.length; i++) {
-            colors[i] = givenColors[i];
+        for (String color : colors) {
+            char charArray[] =  color.toCharArray();
+
+            if (charArray[0] == charArray[1] && charArray[1] == charArray[2]) {
+
+            } else if (charArray[0] == charArray[1]) {
+                char temp = charArray[2];
+                charArray[2] = charArray[1];
+                charArray[1] = temp;
+            } else if (charArray[1] == charArray[2]) {
+                char temp = charArray[1];
+                charArray[1] = charArray[0];
+                charArray[0] = temp;
+            }
+
+            if (count.get(charArray[0]) == null) {
+                count.put(charArray[0], 0);
+            }
+
+            count.put(charArray[0], count.get(charArray[0]) + 1);
+
+            answer = Math.max(answer, count.get(charArray[0]));
         }
 
-        return 3;
+        return answer;
     }
-
 
     public static void main(String[] args) {
         AbcProblem ap = new AbcProblem();
