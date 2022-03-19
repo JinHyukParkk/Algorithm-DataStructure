97b2da649a11873fae5771906b4a6153ec90c085
hyuk <pjh08190819@naver.com>
2022-03-20 00:14:45 +0900

    feat: OpenChat 풀이

--- programmers/OpenChat.java
+++ programmers/OpenChat.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 public class OpenChat {
     
+    private static final String ENTER = "Enter";
+    private static final String LEAVE = "Leave";
+    private static final String ENTER_FORMAT = "님이 들어왔습니다.";
+    private static final String LEAVE_FORMAT = "님이 나갔습니다.";
+    
+
+
     class Command {
         String action;
         String id;
@@ -16,33 +23,43 @@ public class OpenChat {
     }
 
     public String[] solution(String[] record) {
-        String[] answer = {};
 
-        List<Command> list = new ArrayList<>();
+        List<Command> commandList = new ArrayList<>();
         Map<String, String> idStore = new HashMap<>();
+        List<String> result = new ArrayList<>();
 
         for (String s : record) {
             String[] sa = s.split("\\s+");
 
             String action = sa[0];
             String id = sa[1];
-            String nick = sa[2];
-
-            if (action.equals("Enter") || action.equals("Leave")) {
-                Command command = new Command(action, id);
-                list.add(command);
-                
-                if (idStore.get(id) == null) {
-                    idStore.put(id, nick);
-                }
-            } else {
-                idStore.put(id, nick);
-            }
-
+            String nick = "";
             
+
+            if (action.equals(ENTER)) {
+                Command command = new Command(action, id);
+                commandList.add(command);
+                nick = sa[2];
+                idStore.put(id, nick);
+            } else if (action.equals(LEAVE)) {
+                Command command = new Command(action, id);
+                commandList.add(command);
+            }
+            else {
+                nick = sa[2];
+                idStore.put(id, nick);
+            }            
         }
 
-        return answer;
+        for (Command command : commandList) {
+            if (command.action.equals(ENTER)) {
+                result.add(idStore.get(command.id) + ENTER_FORMAT);
+            } else if (command.action.equals(LEAVE)) {
+                result.add(idStore.get(command.id) + LEAVE_FORMAT);
+            }
+        }
+
+        return result.toArray(new String[result.size()]);
     }
 
     public static void main(String[] args) {
@@ -51,7 +68,7 @@ public class OpenChat {
         String[] givenArr = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
         
         for (String s : openChat.solution(givenArr)) {
-
+            System.out.println(s);
         }
     }
 }
