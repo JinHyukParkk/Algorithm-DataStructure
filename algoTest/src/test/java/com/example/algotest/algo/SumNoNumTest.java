67b8c8db22d998df805a2d795594367b0cc08db6
hyuk <pjh08190819@naver.com>
2022-08-10 00:13:14 +0900

    test : 테스트 검증

--- algoTest/src/test/java/com/example/algotest/algo/SumNoNumTest.java
+++ algoTest/src/test/java/com/example/algotest/algo/SumNoNumTest.java
@@ -1,7 +1,26 @@
 package com.example.algotest.algo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SumNoNumTest {
 
+    SumNoNum sumNoNum = new SumNoNum();
+
+    @Test
+    void case1() {
+        int[] numbers = new int[]{1,2,3,4,6,7,8,0};
+
+        int answer = sumNoNum.solution2(numbers);
+        assertThat(answer).isEqualTo(14);
+    }
+
+    @Test
+    void case2() {
+        int[] numbers = new int[]{5,8,4,0,6,7,9};
+
+        int answer = sumNoNum.solution(numbers);
+        assertThat(answer).isEqualTo(6);
+    }
 }
