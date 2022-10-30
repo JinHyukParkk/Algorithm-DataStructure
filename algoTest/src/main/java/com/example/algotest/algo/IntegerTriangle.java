2c445067c3d7c8d04ea649e3baf1defe1ab8f0a7
hyuk <pjh08190819@naver.com>
2022-10-30 22:45:23 +0900

    study : 트라이앵글 DP 업데이트

--- algoTest/src/main/java/com/example/algotest/algo/IntegerTriangle.java
+++ algoTest/src/main/java/com/example/algotest/algo/IntegerTriangle.java
@@ -3,18 +3,26 @@ package com.example.algotest.algo;
 import java.util.Arrays;
 
 public class IntegerTriangle {
+
     public int solution(int[][] triangle) {
+
         int[][] dp = new int[triangle.length + 1][triangle.length + 1];
 
         dp[0][0] = triangle[0][0];
         for (int i = 1; i < triangle.length; i++) {
-            dp[i][0] = triangle[i][0] + dp[i-1][0];
-            dp[i][i] = triangle[i][i] + dp[i-1][i-1];
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1], dp[i-1][j]);
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (j == 0) {
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j];
+                } else if (j == triangle[i].length) {
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+                }
             }
         }
 
-        return Arrays.stream(dp[triangle.length - 1]).max().getAsInt();
+        return Arrays.stream(dp[triangle.length - 1])
+                .max()
+                .getAsInt();
     }
 }
